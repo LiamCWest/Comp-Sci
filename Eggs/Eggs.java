02450ef233f404f303b0c16d59105c353611ae67
LiamCWest <764394@pdsb.net>
2023-02-22 15:53:37 -0500

    Eggs

---
+++ Eggs/Eggs.java
@@ -0,0 +1,14 @@
+package Eggs;
+
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Eggs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many eggs?");
+        int eggs = sc.nextInt();
+        System.out.printf("That is %d crate(s), %d carton(s), and %d egg(s) leftover.", (int) Math.floor(eggs/180), (int) Math.floor((eggs%180)/12), (int) Math.floor((eggs%180)%12));
+        sc.close();
+    }
+}
