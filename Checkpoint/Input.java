eb2cc484bf6cca2ccf1c63e55e92f90d34222f44
LiamCWest <764394@pdsb.net>
2023-02-22 20:48:41 -0500

    Stocks and Issue

--- Checkpoint/Input.java
+++ Checkpoint/Input.java
@@ -10,13 +10,19 @@ class Input{
         Scanner sc = new Scanner(System.in);
 
         //Question 1
-        // Madlibs("There once was a person named __NAME__ who lived in __CITY__. At the age of __AGE__, __NAME__ went to college at __COLLEGE__. __NAME__ graduated and went to work as a __PROFESSION__. Then, __NAME__ adopted a(n) __ANIMAL__ named __PETNAME__. They both lived happily ever after!");
+        Madlibs("There once was a person named __NAME__ who lived in __CITY__. At the age of __AGE__, __NAME__ went to college at __COLLEGE__. __NAME__ graduated and went to work as a __PROFESSION__. Then, __NAME__ adopted a(n) __ANIMAL__ named __PETNAME__. They both lived happily ever after!");
 
         //Question 2
         Salary(sc);
 
         //Question 3
         Items(sc);
+
+        //Question 4
+        Stocks(600, 21.77, 2);
+
+        //Question 5
+        Issue();
 
         sc.close();
     }
@@ -59,6 +65,14 @@ class Input{
         System.out.printf("The salary is %s\n", money.format(hours*wage));
     }
 
+    //Question 4
+    static void Stocks(int shares, double price, int commission){
+        NumberFormat money = NumberFormat.getCurrencyInstance();
+        double totalCost = shares*price;
+        double commissionFee = totalCost*((double) commission/100);
+        System.out.printf("Amount paid for stock alone: %s\nCommission fee: %s\nTotal amount paid: %s\n", money.format(totalCost), money.format(commissionFee), money.format(commissionFee+totalCost));
+    }
+
     //Question 3
     static void Items(Scanner sc){
         System.out.println("Enter the number of items purchased\nfollowed by the cost of one item.");
@@ -68,5 +82,17 @@ class Input{
         System.out.printf("%1d items at %s each.\n", numItems, money.format(itemCost));
         System.out.printf("Total amount due %s.\nPlease take your merchandise.\n", money.format(itemCost*numItems));
         System.out.printf("Place %s in an envelope\nand slide it under the office door.\nThank you for using the self-service line.\n", money.format(itemCost*numItems));
+    }
+
+    //Question 5
+    static void Issue(){
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter your age.");
+        int age = keyboard.nextInt();
+        System.out.println("Enter your name.");
+        // keyboard.nextLine() does not work, keyboard.next() is much better
+        String name = keyboard.next();
+        System.out.println(name + ",you are " + age + " years old.");
+        keyboard.close();
     }
 }
