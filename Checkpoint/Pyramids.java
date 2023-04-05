ac70c2de75ea4000a033b95d2dac8ec9dbae5d02
LiamCWest <764394@pdsb.net>
2023-04-05 12:46:48 -0400

    one liner

--- Checkpoint/Pyramids.java
+++ Checkpoint/Pyramids.java
@@ -2,16 +2,7 @@ package Checkpoint;
 
 public class Pyramids {
     public static void main(String[] args) {
-        for(int i = 1; i<18; i+=2){
-            System.out.print(Line(' ', 18-i));
-            for(int s = 0; s<i; s++){
-                System.out.print("* ");
-            }
-            System.out.print("\n");
-        }
-    }
-
-    static String Line(Character c, int i){
-        return new String(new char[i]).replace('\0', c);
+        for(int i = 0; i<5; i++)
+            System.out.println("  ".repeat(6-i)+"* ".repeat((i*2)+1));
     }
 }
