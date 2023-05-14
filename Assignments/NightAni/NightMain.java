0defdc07132b997df6614c5aaa470515965dee6b
LiamCWest <764394@pdsb.net>
2023-05-14 00:22:15 -0400

    BUILDINGGGG WITH CARR

--- Assignments/NightAni/NightMain.java
+++ Assignments/NightAni/NightMain.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 public class NightMain extends JFrame{
     private ArrayList<ArrayList<Building>> buildings;
     private Building mainBuilding;
+    private Car car;
     public static void main(String[] args) {
         new NightMain();
     }
@@ -17,15 +18,16 @@ public class NightMain extends JFrame{
         setTitle("Welcome to Java");
         //Set the size of the window
         setSize(400, 400);
-        this.buildings = GenBuildings(1);
+        this.buildings = GenBuildings();
+        this.car = new Car(new int[]{25, 300}, new int[]{1, 1}, new Color(255, 0, 0, 255));
         //Set the close operation
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Set the visibility
         setVisible(true);
     }
 
+
     public void paint(Graphics g){
-        // super.paint(g);
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -38,7 +40,6 @@ public class NightMain extends JFrame{
 
             drawRoad(g, (this.getSize().width/200)*t);
             
-            
             for (int i = 2; i > 0; i--){
                 for (Building building : this.buildings.get(i-1)) {
                     int [] pos = building.GetPos();
@@ -47,7 +48,8 @@ public class NightMain extends JFrame{
                     building.MoveBuilding(new int[]{pos[0] - (this.getSize().width/200)+((i-1)*1), pos[1]});
                 }
             }
-            drawMainBuilding(g, (this.getSize().width/200)*t);
+            drawMainBuilding(g, true);
+            drawCar(g, false);
             t++;
             try {
                 Thread.sleep(30);
@@ -55,10 +57,40 @@ public class NightMain extends JFrame{
                 e.printStackTrace();
             }
         }
+
+        for(int n = 0; n < 60; n++){
+            g.setColor(new Color(0,0,0,255));
+            g.fillRect(0, 0, this.getSize().width+100, this.getSize().height+100);
+
+            drawRoad(g, (this.getSize().width/200)*t);
+            
+            for (int i = 2; i > 0; i--){
+                for (Building building : this.buildings.get(i-1)) {
+                    int [] pos = building.GetPos();
+                    if((pos[0] <= this.getSize().width) && (pos[0] >= 0-building.GetWidth())) building.DrawBuilding(g);
+                }
+            }
+            drawMainBuilding(g, false);
+            drawCar(g, true);
+            try {
+                Thread.sleep(30);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        /*
+        car door = true
+        draw car with door
+        human walk out
+        car door = false
+        draw car without door
+        main building doors open
+        human walk in to main building
+        main building doors close
+        */
     }
 
-    public ArrayList<ArrayList<Building>> GenBuildings(int numBuildings){
-
+    public ArrayList<ArrayList<Building>> GenBuildings(){
         ArrayList<ArrayList<Building>> buildings = new ArrayList<ArrayList<Building>>(Arrays.asList(new ArrayList<Building>(),new ArrayList<Building>(),new ArrayList<Building>()));
 
         int height = this.getSize().height;
@@ -72,7 +104,7 @@ public class NightMain extends JFrame{
 
             Double buildingMultiplier = 1.0;
             Double WMulti = 1.6;
-
+        
             while(offset < (width+(0.075*width))*2-(i*width+(0.075*width))){
                 int sizeWidth = (int)Math.round((Math.random()*(0.075*width*buildingMultiplier*WMulti))+(0.05*width*buildingMultiplier*WMulti)+15);
                 int sizeHeight = (int)Math.round((Math.random()*(0.1*height*buildingMultiplier))+(0.3*height*buildingMultiplier)+(i*height*0.05));
@@ -82,21 +114,18 @@ public class NightMain extends JFrame{
                 int G = (int)(Math.random()*20)+61;
                 int B = (int)(Math.random()*40)+115;
                 Color color = new Color(R, G, B, 255);
-
-                buildings.get(i).add(new Building(new int[]{offset+this.getSize().width,height-size[1]-(int)(0.2*this.getSize().height)}, size, color));
+                buildings.get(i).add(new Building(new int[]{offset+this.getSize().width,height-size[1]-(int)(0.2*this.getSize().height)}, size, color, false, new int[]{0,0}, 0, new Color(0,0,0,0), 0, new Color(0,0,0,0)));
                 offset += sizeWidth;
             }
             if(i == 0){                
                 int mainHeight = 250;
                 int mainWidth = 100;
-
-                int mainX = 1118;
+                int mainX = 1110;
                 int mainY = this.getSize().height-mainHeight-(int)(0.2*this.getSize().height);
 
-                this.mainBuilding = new Building(new int[]{mainX, mainY}, new int[]{mainWidth, mainHeight}, new Color(255, 255, 255, 255));
+                this.mainBuilding = new Building(new int[]{mainX, mainY}, new int[]{mainWidth, mainHeight}, new Color(255, 0, 255, 255), true, new int[]{25,25}, 5, new Color(255,255,255,255), 2, new Color(0,0,255,255));
             }
         }
-
         return buildings;
     }
 
@@ -112,12 +141,22 @@ public class NightMain extends JFrame{
         }
     }
 
-    public void drawMainBuilding(Graphics g, int move){
+    public void drawMainBuilding(Graphics g, Boolean move){
         int[] pos = this.mainBuilding.GetPos();
         if((pos[0] <= this.getSize().width) && (pos[0] >= 0-this.mainBuilding.GetWidth())) this.mainBuilding.DrawBuilding(g);
-        pos = this.mainBuilding.GetPos();
-        System.out.println(pos[0] + " " + pos[1]);
-        this.mainBuilding.MoveBuilding(new int[]{pos[0] - (this.getSize().width/200), pos[1]});
+        if(move){
+            pos = this.mainBuilding.GetPos();
+            this.mainBuilding.MoveBuilding(new int[]{pos[0] - (this.getSize().width/200), pos[1]});
+        }
+    }
+
+    public void drawCar(Graphics g, Boolean move){
+        int[] pos = this.car.GetPos();
+        this.car.DrawCar(g);
+        if(move){
+            pos = this.car.GetPos();
+            this.car.MoveCar(new int[]{pos[0]+1, pos[1]});
+        }
     }
 
     public static int PositiveMod(int value, int mod){
@@ -128,12 +167,36 @@ public class NightMain extends JFrame{
 class Building {
     private int[] pos;
     private int[] size;
+    private int[] windowSize;
+    private int gap;
     private Color color;
+    private Color windowColor;
+    private Boolean windowsQ;
+    private int doors;
+    private Color doorColor;
 
-    public Building(int[] pos, int[] size, Color color){
+    private int[] windows;
+    private int wallGap;
+    private int doorGap;
+
+    public Building(int[] pos, int[] size, Color color, Boolean windowsQ, int[] windowSize, int gap, Color windowColor, int doors, Color doorColor){
         this.pos = pos;
         this.size = size;
+        this.windowSize = windowSize;
+        this.gap = gap;
         this.color = color;
+        this.windowColor = windowColor;
+        this.windowsQ = windowsQ;
+        this.doors = doors;
+        this.doorColor = doorColor;
+        if(windowsQ){
+            this.windows = new int[]{(size[0]-gap)/(windowSize[0]+gap), (size[1]-gap)/(windowSize[1]+gap)};
+            this.wallGap = ((size[0])-(((windowSize[0]+gap)*windows[0])))/2;
+        }
+        if(doors > 0){
+            this.windows[1] -= 2;
+            this.doorGap = ((size[0])-(((windowSize[0]+gap)*doors)))/2;
+        }
     }
 
     public void DrawBuilding(Graphics g){
@@ -141,6 +204,24 @@ class Building {
         g.fillRect(pos[0], pos[1], size[0], size[1]);
         g.setColor(Color.BLACK);
         g.drawRect(pos[0], pos[1], size[0], size[1]);
+        
+        if(windowsQ){
+            g.setColor(windowColor);
+            for(int i = 0; i < windows[0]; i++){
+                for(int j = 0; j < windows[1]; j++){
+                    g.fillRect(pos[0]+((windowSize[0]+gap)*i)+wallGap+(int)Math.round(gap/2), pos[1]+((windowSize[1]+gap)*j)+gap+(int)Math.round(gap/2), windowSize[0], windowSize[1]);
+                }
+            }
+        }
+
+        if(doors > 0){
+            g.setColor(doorColor);
+            for(int i = 0; i < doors; i++){
+                int x = pos[0]+doorGap+(i*(windowSize[1]+gap));
+                int y = pos[1]+(windowSize[1]+gap)*(windows[1])+wallGap+gap;
+                g.fillRect(x, y, windowSize[0], (windowSize[1]*2)+gap);
+            }
+        }
     }
 
     public void MoveBuilding(int[] newPos){
@@ -153,5 +234,37 @@ class Building {
 
     public int GetWidth(){
         return this.size[0];
+    }
+}
+
+class Car {
+    private int[] pos;
+    private int[] size;
+    private Color carColor;
+    public Car(int[] pos, int[] size, Color carColor){
+        this.pos = pos;
+        this.size = size;
+        this.carColor = carColor;
+    }
+
+    public void DrawCar(Graphics g){
+        g.setColor(this.carColor);
+        g.fillRect(pos[0], pos[1]+(10*size[1]), 80*size[0], 40*size[1]);
+        g.fillRect(pos[0]+(10*size[0]), pos[1], 100*size[0], 10*size[1]);
+        g.fillRect(pos[0]+(80*size[0]), pos[1]+(25*size[1]), 55*size[0], 25*size[1]);
+        g.setColor(new Color(0,100,0,255));
+        g.fillRect(pos[0]+(80*size[0]), pos[1]+(10*size[1]), 40*size[0], 15*size[1]);
+
+        g.setColor(new Color(50,50,50,255));
+        g.fillOval(pos[0]+(10*size[0]), pos[1]+(35*size[1]), 25*size[0], 25*size[1]);
+        g.fillOval(pos[0]+(100*size[0]), pos[1]+(35*size[1]), 25*size[0], 25*size[1]);
+    }
+
+    public void MoveCar(int[] newPos){
+        this.pos = newPos;
+    }
+
+    public int[] GetPos(){
+        return this.pos;
     }
 }
