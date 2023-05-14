e19c4c0bd507588fdf0ab40ec731899f78223244
archiso7 <80644608+archiso7@users.noreply.github.com>
2023-05-14 16:31:52 -0400

    lots of stuff

--- Assignments/NightAni/NightMain.java
+++ Assignments/NightAni/NightMain.java
@@ -2,102 +2,124 @@ package Assignments.NightAni;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class NightMain extends JFrame{
     private ArrayList<ArrayList<Building>> buildings;
     private Building mainBuilding;
     private Car car;
+    private int aniSpeed;
+    private int layers;
+    private Human human;
+
+    private BufferedImage offScreenBuffer;
+
     public static void main(String[] args) {
-        new NightMain();
+        SwingUtilities.invokeLater(NightMain::new);
     }
 
     public NightMain(){
+        this.aniSpeed = 10;
+        this.layers = 2;
+
         //Set the title
         setTitle("Welcome to Java");
         //Set the size of the window
         setSize(400, 400);
-        this.buildings = GenBuildings();
+        this.buildings = GenBuildings(this.layers);
         this.car = new Car(new int[]{25, 300}, new int[]{1, 1}, new Color(255, 0, 0, 255));
+        this.human = new Human(new int[]{30, 310}, new int[]{50,50}, Color.BLUE, Color.CYAN);
         //Set the close operation
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         //Set the visibility
         setVisible(true);
+
+        // Create the off-screen buffer with the same size as the window
+        offScreenBuffer = new BufferedImage(getSize().width, getSize().height, BufferedImage.TYPE_INT_ARGB);
+        startAnimation();
     }
 
-
-    public void paint(Graphics g){
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        int t = 0;
-        while(buildings.get(1).get(0).GetPos()[0] > -10){
-            g.setColor(new Color(0,0,0,255));
-            g.fillRect(0, 0, this.getSize().width+100, this.getSize().height+100);
-
-            drawRoad(g, (this.getSize().width/200)*t);
-            
-            for (int i = 2; i > 0; i--){
-                for (Building building : this.buildings.get(i-1)) {
-                    int [] pos = building.GetPos();
-                    if((pos[0] <= this.getSize().width) && (pos[0] >= 0-building.GetWidth())) building.DrawBuilding(g);
-                    pos = building.GetPos();
-                    building.MoveBuilding(new int[]{pos[0] - (this.getSize().width/200)+((i-1)*1), pos[1]});
+    private void startAnimation() {
+        Thread animationThread = new Thread(() -> {
+            int t = 0;
+            //first part of animation, moving background buildings
+            while (buildings.get(1).get(0).GetPos()[0] > -10) {
+                renderFrame(t, 0);
+                t++;
+                try {
+                    Thread.sleep(aniSpeed);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-            drawMainBuilding(g, true);
-            drawCar(g, false);
-            t++;
+            
+            //second part of animation, moving car
+            for (int n = 0; n < 100; n++) {
+                renderFrame(t, 1);
+                try {
+                    Thread.sleep(aniSpeed);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            //wait
             try {
-                Thread.sleep(30);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-        }
 
-        for(int n = 0; n < 60; n++){
-            g.setColor(new Color(0,0,0,255));
-            g.fillRect(0, 0, this.getSize().width+100, this.getSize().height+100);
-
-            drawRoad(g, (this.getSize().width/200)*t);
-            
-            for (int i = 2; i > 0; i--){
-                for (Building building : this.buildings.get(i-1)) {
-                    int [] pos = building.GetPos();
-                    if((pos[0] <= this.getSize().width) && (pos[0] >= 0-building.GetWidth())) building.DrawBuilding(g);
+            //third part of animation, moving human to building
+            for (int n = 0; n < 155; n++) {
+                renderFrame(t, 2);
+                try {
+                    Thread.sleep(aniSpeed*2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
             }
-            drawMainBuilding(g, false);
-            drawCar(g, true);
-            try {
-                Thread.sleep(30);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+
+            //fourth part of animation, moving human to doors
+            for (int n = 0; n < 55; n++) {
+                renderFrame(t, 3);
+                try {
+                    Thread.sleep(aniSpeed*2);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
-        }
-        /*
-        car door = true
-        draw car with door
-        human walk out
-        car door = false
-        draw car without door
-        main building doors open
-        human walk in to main building
-        main building doors close
-        */
+            renderFrame(t, 4);
+        });
+        animationThread.start();
     }
 
-    public ArrayList<ArrayList<Building>> GenBuildings(){
-        ArrayList<ArrayList<Building>> buildings = new ArrayList<ArrayList<Building>>(Arrays.asList(new ArrayList<Building>(),new ArrayList<Building>(),new ArrayList<Building>()));
+    private void renderFrame(int t, int part) {
+        Graphics offScreenGraphics = offScreenBuffer.getGraphics();
+        offScreenGraphics.setColor(new Color(0, 0, 0, 255));
+        offScreenGraphics.fillRect(0, 0, this.getSize().width + 100, this.getSize().height + 100);
+
+        drawRoad(offScreenGraphics, (this.getSize().width / 200) * t);
+        drawBuilding(offScreenGraphics, part == 0);
+        drawMainBuilding(offScreenGraphics, part == 0);
+        drawCar(offScreenGraphics, part == 1);
+        drawHuman(offScreenGraphics, part);
+
+        Graphics frameGraphics = getGraphics();
+        frameGraphics.drawImage(offScreenBuffer, 0, 0, this);
+        offScreenGraphics.dispose();
+    }
+
+    public ArrayList<ArrayList<Building>> GenBuildings(int layers){
+        ArrayList<ArrayList<Building>> buildings = new ArrayList<ArrayList<Building>>();
 
         int height = this.getSize().height;
         int width = this.getSize().width;
 
         // random buildings
-        for(int i = 0; i < 2; i++){
+        for(int i = 0; i < layers; i++){
+            buildings.add(new ArrayList<Building>());
             int offset = 0;
             width -= i*(width*0.05);
             height -= i*(height*0.05);
@@ -123,14 +145,29 @@ public class NightMain extends JFrame{
                 int mainX = 1110;
                 int mainY = this.getSize().height-mainHeight-(int)(0.2*this.getSize().height);
 
-                this.mainBuilding = new Building(new int[]{mainX, mainY}, new int[]{mainWidth, mainHeight}, new Color(255, 0, 255, 255), true, new int[]{25,25}, 5, new Color(255,255,255,255), 2, new Color(0,0,255,255));
+                this.mainBuilding = new Building(new int[]{mainX, mainY}, new int[]{mainWidth, mainHeight}, new Color(138, 137, 114, 255), true, new int[]{25,25}, 5, new Color(201,199, 83,255), 2, new Color(120,119,95,255));
             }
         }
         return buildings;
     }
 
+    public void drawBuilding(Graphics g, Boolean move){
+        for (int i = this.layers; i > 0; i--) {
+            for (Building building : this.buildings.get(i - 1)) {
+                int[] pos = building.GetPos();
+                if ((pos[0] <= this.getSize().width) && (pos[0] >= 0 - building.GetWidth())){
+                    building.DrawBuilding(g);
+                }
+                if(move){
+                    pos = building.GetPos();
+                    building.MoveBuilding(new int[]{pos[0] - (this.getSize().width / 200) + ((i - 1) * 1), pos[1]});
+                }
+            }
+        }
+    }
+
     public void drawRoad(Graphics g, int move){
-        g.setColor(new Color(0, 0,0 ,255));
+        g.setColor(new Color(82, 82,82 ,255));
         g.fillRect(0, this.getSize().height-((int)(0.2*this.getSize().height)), this.getSize().width, (int)(0.2*this.getSize().height));
 
         //road lines
@@ -156,6 +193,23 @@ public class NightMain extends JFrame{
         if(move){
             pos = this.car.GetPos();
             this.car.MoveCar(new int[]{pos[0]+1, pos[1]});
+        }
+    }
+
+    public void drawHuman(Graphics g, int move){
+        if(move == 2){
+            this.human.DrawHuman(g);
+            int[] humanPos = this.human.GetPos();
+            this.human.MoveHuman(new int[]{humanPos[0]+1, humanPos[1]});
+        }
+        else if(move == 3){
+            this.human.DrawHuman(g);
+            int[] humanPos = this.human.GetPos();
+            this.human.MoveHuman(new int[]{humanPos[0], humanPos[1]-1});
+        }
+        else{
+            int[] carPos = this.car.GetPos();
+            this.human.MoveHuman(new int[]{carPos[0]+30, carPos[1]});
         }
     }
 
@@ -265,6 +319,81 @@ class Car {
     }
 
     public int[] GetPos(){
+        return this.pos;
+    }
+}
+
+class Human {
+    private int[] pos;
+    private int[] size;
+    private Color color;
+    private Color headColor;
+
+    public Human(int[] pos, int[] size, Color color, Color headColor) {
+        this.pos = pos;
+        this.size = size;
+        this.color = color;
+        this.headColor = headColor;
+    }
+
+    public void DrawHuman(Graphics g) {
+        g.setColor(color);
+        int bodyWidth = size[0] / 3;
+        int bodyHeight = (int) (size[1] * 0.5);
+        int armWidth = 5;
+        int armHeight = size[1] / 3;
+        int legWidth = bodyWidth / 3;
+        int legHeight = size[1] / 3;
+        int armAngle = 215;
+
+        // Body
+        int bodyX = pos[0] + (size[0] / 2) - (bodyWidth / 2);
+        int bodyY = pos[1] + (size[1] - bodyHeight);
+        g.fillOval(bodyX, bodyY, bodyWidth, bodyHeight);
+
+        // Head
+        int headSize = size[1] / 4;
+        int headX = pos[0] + (size[0] / 2) - (headSize / 2);
+        int headY = bodyY - headSize;
+        g.setColor(headColor);
+        g.fillOval(headX, headY, headSize, headSize);
+
+        // Arms
+        int shoulderY = bodyY+5;
+        int shoulderX = bodyX + (bodyWidth / 2);
+
+        // Convert the arm angle to radians
+        double armAngleRad = Math.toRadians(armAngle);
+
+        // Calculate the end points of the arms
+        int leftArmEndX = (int) (shoulderX - armHeight * Math.cos(armAngleRad));
+        int leftArmEndY = (int) (shoulderY - armHeight * Math.sin(armAngleRad));
+
+        int rightArmEndX = (int) (shoulderX + armHeight * Math.cos(armAngleRad));
+        int rightArmEndY = (int) (shoulderY - armHeight * Math.sin(armAngleRad));
+
+        // Set the arm width
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.setStroke(new BasicStroke(armWidth));
+
+        // Draw the arms
+        g2d.setColor(color);
+        g2d.drawLine(shoulderX, shoulderY, leftArmEndX, leftArmEndY);
+        g2d.drawLine(shoulderX, shoulderY, rightArmEndX, rightArmEndY);
+    
+
+        // Legs
+        int legY = bodyY + bodyHeight;
+        g.fillRect(bodyX, legY, legWidth, legHeight);
+        g.fillRect(bodyX + bodyWidth - legWidth, legY, legWidth, legHeight);
+    }
+
+
+    public void MoveHuman(int[] newPos) {
+        this.pos = newPos;
+    }
+
+    public int[] GetPos() {
         return this.pos;
     }
 }
